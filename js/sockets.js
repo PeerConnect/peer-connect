9de91b3062c3f85e49f4653a62ef23c08740b94d
mikeg <mikeg@steelhouse.com>
2018-01-06 14:44:09 -0800

    sockets & webrtc communication set up

--- js/sockets.js
+++ js/sockets.js
@@ -1,3 +1,4 @@
+const Peer = SimplePeer;
 // indicates if client has downloaded assets from server
 // and can send assets to new client connections
 let initiator;
@@ -11,9 +12,9 @@ const socket = io.connect();
 // client has loaded page
 socket.emit("create");
 
-// when new socket has been opend
-socket.on("created", () => {
-  console.log("socket.on CREATED");
+// when first peer socket has been opend
+socket.on("created", numClients => {
+  console.log(`socket.on CREATED:  ${numClients}`);
   // created client can initiate downloads to other clients
   initiator = true;
   loadAssetsFromServer();
@@ -33,5 +34,51 @@ function loadAssetsFromServer() {
     image1.setAttribute("src", "../assets/image1.jpg");
     image2.setAttribute("src", "../assets/image2.png");
     image3.setAttribute("src", "../assets/image3.jpg");
+    assetsDownloaded = true;
   }
+}
+
+// // when new peer socket has been opend
+socket.on("new_peer", data => {
+  // const localConn = new RTCPeerConnection();
+  // const remoteConn = new RTCPeerConnection();
+  console.log(`NEW PEER JOINED. assetsDownloaded: ${assetsDownloaded}`);
+  // localConn
+  //   .createOffer()
+  //   .then(offer =>
+  //     localConn.setLocalDescription(new RTCSessionDescription(offer))
+  //   )
+  //   .then(() => remoteConn.setRemoteDescription(localConn.localDescription))
+  //   .then(() => remoteConn.createAnswer())
+  //   .then(answer =>
+  //     remoteConn.setLocalDescription(new RTCSessionDescription(answer))
+  //   )
+  //   .then(() => localConn.setRemoteDescription(remoteConn.localDescription));
+
+  // const configuration = {
+  //   iceServers: [{ url: "stun:stun.l.google.com:19302" }]
+  // };
+  const p = new Peer({ initiator: assetsDownloaded, trickle: false });
+  p.on("error", err => {
+    console.log(`error: ${err}`);
+  });
+
+  p.on("signal", data => {
+    console.log(`SIGNAL: ${JSON.stringify(data)}`);
+    sendMessage(JSON.stringify(data));
+  });
+});
+
+socket.on("message", message => {
+  console.log(`message:  ${message}`);
+  p.signal(JSON.parse(message));
+});
+
+// sends a message back to the singaling server
+function sendMessage(message) {
+  socket.emit("message", message);
+}
+
+function downloadAssetsFromPeer() {
+  console.log("LOAD ASSETS FROM PEER");
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const socket = require("socket.io");
+// const Peer = require("simple-peer");
+// const wrtc = require("wrtc");
 
 // App setup
 const PORT = 8080;
@@ -24,11 +26,13 @@ app.use((req, res, next) => {
 
 // Sockets setup
 const io = socket(server);
+// Store list of all clients actively using app
 const activeClients = [];
+let numClients;
 
 io.on("connection", socket => {
   console.log(`socket connection started. ID: ${socket.id}`);
-  activeClients.push(socket.id);
+  activeClients.push(socket);
   console.log(`activeClients: ${activeClients}`);
 
   socket.on("disconnect", () => {
@@ -40,11 +44,27 @@ io.on("connection", socket => {
   socket.on("create", () => {
     console.log("socket.on CREATE");
     // keep track of how many clients are active
-    const numClients = activeClients.length;
+    numClients = activeClients.length;
 
-    // if client is first to connect
+    // create new peer
+    // const p = new Peer({
+    //   initiator: numClients === 1 ? true : false,
+    //   wrtc: wrtc
+    // });
+
     if (numClients === 1) {
-      socket.emit("created");
+      // if client is first to connect
+      socket.emit("created", numClients);
+    } else {
+      // socket.emit("new_peer", numClients, socket.id);
+      io.sockets.emit("new_peer", { peer: socket.id });
     }
+
+    // on receiving a message, broadcast it to every socket
+    socket.on("message", message => {
+      // log the message on the console
+      console.log("Client said:", message);
+      activeClients[1].emit("message", message);
+    });
   });
 });

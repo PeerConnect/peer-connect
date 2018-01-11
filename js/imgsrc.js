e60bead390df8f90705028612ec8e7f9704cb132
jiminykbob <jikang92@hotmail.com>
2018-01-10 21:24:42 -0800

    send multiple assets p2p

---
+++ js/imgsrc.js
@@ -0,0 +1,12 @@
+// imageFind();
+
+function imageFind() {
+  const images = document.getElementsByTagName('img');
+  // const imageArray = [];
+  // Object.values(images)
+  // for (let i = 0; i < images.length; i += 1) {
+  //   //give specific data-id to each img tag
+  // }
+  // console.log(images)
+  return images;
+}

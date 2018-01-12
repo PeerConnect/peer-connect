de0439f2d357fa0c2fdc038b13d4cfad95513271
mikeg <mikeg@steelhouse.com>
2018-01-11 20:04:18 -0800

    set threshold to 1 for testing

--- peer-config.js
+++ peer-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // how many peers must be connected before loading assets from peers
   // if threshold = 3, fourth client will load from peers
-  threshold: 3,
+  threshold: 1,
   assetTypes: ['.jpg'],
   lazyLoading: false,
   fold: false

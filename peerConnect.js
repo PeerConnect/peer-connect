af8d8df45911023bfb6905318339820ee589e5ef
David Sally <davidcsally@gmail.com>
2018-01-16 17:30:30 -0800

    [Update] wtf is this tab shit, two spaces plz

--- peerConnect.js
+++ peerConnect.js
@@ -154,15 +154,15 @@ function createReceiverPeer(socket, activeClients, config, serverStats) {
 // function to calculate distance using two sets of coordindates
 // source: https://www.geodatasource.com/developers/javascript
 function distance(lat1, lon1, lat2, lon2) {
-	const radlat1 = Math.PI * lat1/180;
-	const radlat2 = Math.PI * lat2/180;
-	const theta = lon1-lon2;
-	const radtheta = Math.PI * theta/180;
-	let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	dist = Math.acos(dist);
-	dist = dist * 180/Math.PI;
-	dist = dist * 60 * 1.1515;
-	return dist;
+  const radlat1 = Math.PI * lat1/180;
+  const radlat2 = Math.PI * lat2/180;
+  const theta = lon1-lon2;
+  const radtheta = Math.PI * theta/180;
+  let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  dist = Math.acos(dist);
+  dist = dist * 180/Math.PI;
+  dist = dist * 60 * 1.1515;
+  return dist;
 };
 
 module.exports = PeerConnect;

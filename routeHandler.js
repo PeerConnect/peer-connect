4bc86f1fb3969694bef97f8af17dfdb884c9c054
justin.ko43 <justin.ko43@gmail.com>
2018-01-20 14:31:53 -0800

    able to loop through assets folder to create video routes

---
+++ routeHandler.js
@@ -0,0 +1,18 @@
+module.exports = function (peerConfig, app) {
+  const fs = require('fs');
+  const path = require('path');
+  const route = peerConfig.videoRoute;
+  
+  fs.readdir(path.join(__dirname, route), (err, files) => {
+    if (err) {
+      console.log(err);
+    }
+
+    files.forEach(file => {
+      console.log(file);
+      app.get(`/video/${file}`, (req, res) => {
+        res.sendFile(path.join(__dirname, route, file));
+      })
+    })
+  })
+}
--- videoConnect.js
+++ videoConnect.js
@@ -1,21 +1,25 @@
 module.exports = function (socket) {
   const createTorrent = require('create-torrent');
-  const parseTorrent = require('parse-torrent');
   const fs = require('fs');
   const videoRoute = './assets/torrent';
 
-  if (fs.existsSync(videoRoute)) {
-    socket.emit('magnet_uri');
-  }
-
-  else {
-    createTorrent(videoRoute, {urlList: ['https://webseed.btorrent.xyz/timedrift-alpine-4k-timelapse.mp4']}, (err, torrent) => {
+  // if (fs.existsSync(videoRoute)) {
+  //   socket.emit('magnet_uri');
+  // }
+  //mp4 route used here
+  // else {
+    createTorrent('./assets/videos', 
+      {urlList: [//'https://webseed.btorrent.xyz/agitation-new-zealand-4k.mp4',
+                 //'https://webseed.btorrent.xyz/timedrift-alpine-4k-timelapse.mp4',
+                 'https://webseed.btorrent.xyz/']}, 
+    (err, torrent) => {
       if (err) {
         throw err;
       }
-      fs.writeFile('myVideo.torrent', torrent);
+
+      fs.writeFile(__dirname + '/assets/torrent/myVideos.torrent', torrent);
   
       socket.emit('magnet_uri');
     });
-  };
+  // };
 }
